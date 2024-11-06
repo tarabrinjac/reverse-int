976031ef48e29fcf14051b980ed7891a2830bc0b
tarabrinjac <tarabrinjac@gmail.com>
2024-11-06 18:36:21 +0600

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+  if (n < 0) {
+    n = -n;
+  }
   
+  const nToString = String(n);
+  const nLastMemberIndex = nToString.length - 1;
+  const arr = [];
+  
+  for (let i = nLastMemberIndex; i >= 0; i--) {
+    arr.push(nToString[i]);
+  }
+  return Number(arr.join(''));
 }
